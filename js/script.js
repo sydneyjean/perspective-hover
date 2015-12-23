02f83487056cc1a41f0a412ea4be9375d75ba1c9
Sydney Hake <sydney@drivedigital.ca>
2015-12-23 14:56:12 -0800

    refining the function so that it will use requestAnimationFrame instead of velocity. Also removing .DS_Store

--- js/script.js
+++ js/script.js
@@ -7,17 +7,20 @@ Inspired by:  http://www.kikk.be/2015/
 Dependencies: Velocity.js (https://github.com/julianshapiro/velocity)
 
 TO DO:
-- add a reset function which brings perspective back to normal
-- separate mousePosition function from perspective function
 - change for loop of parent/els to use a higher order function
- 
+- change function from accepting parameters to having an options object that overwrites defaults 
+- implement requestAnimationFrame so plugin isn't dependent on velocity
+
 */
 
-function perspectiveHover(el, intensity) {
+function perspectiveHover(el, parent, intensity) {
     
     var self         = this,
-        parent       = document.getElementsByClassName('js-perspective'),
-        els          = document.getElementsByClassName('js-perspective-card');
+        elClass      = el.replace(/\./g,''),
+        parentClass  = parent.replace(/\./g,''),
+        parent       = document.getElementsByClassName(parentClass),
+        els          = document.getElementsByClassName(elClass);
+
     
     this.perspective = function(e, el) {
         
@@ -32,19 +35,63 @@ function perspectiveHover(el, intensity) {
         var valY = -( ( elWidth/2 - (mouseX - elX) ) / (elWidth/2) * intensity ),
             valX = ( elHeight/2 - (mouseY - elY) ) / (elHeight/2) * intensity;
 
-        Velocity(el, { rotateX: valX+'deg', rotateY: valY+'deg', }, 0);
+        // Velocity(el, { rotateX: valX+'deg', rotateY: valY+'deg', }, 0);
+        el.style.transform = 'rotateX('+ valX + 'deg) rotateY(' + valY + 'deg)';
 
     }
 
-    this.reset = function(el) {
+    this.reset = function(el, valX) {
 
-        Velocity(el, { rotateX: 0, rotateY: 0, }, 250, [0.175, 0.885, 0.75, 2.5]); // Array vals are easing
+        console.log(valX);
+
+        animate({
+            time: 0.5,  //time in seconds
+            run: function(rate) {
+                // console.log(rate);
+                // el.style.transform = 'rotateX('+ rate +'deg) rotateY('+ rate +'deg)';
+            }
+        });
+
+        function animate(item) {
+
+            var duration = 1000*item.time,
+                end = +new Date() + duration;
+
+            var step = function() {
+
+                var current = +new Date(),
+                    remaining = end - current;
+
+                if( remaining < 60 ) {
+                    item.run(0);  //1 = progress is at 100%
+                    return;
+
+                } else {
+                    // var rate = 1 - remaining/duration;
+                    var rate = remaining/duration;
+                    item.run(rate);
+                }
+                window.requestAnimationFrame(step);
+            }
+            step();
+        }
+
+        // Velocity(el, { rotateX: 0, rotateY: 0, }, 250, [0.175, 0.85, 0.55, 1.2]); // Array vals are easing
+    }
+
+    this.getTransforms = function(string, index) {
+        var regExp = /\(([^)]+)\)/;
+        var matches = string.match(regExp);
+
+        //matches[1] contains the value between the parentheses
+        // console.log(matches[1]);
+        console.log( matches );
     }
 
     for (var i = 0; i < parent.length; i++) {
 
         parent[i].addEventListener('mousemove', function(e) {
-            if ( e.target.classList.contains('js-perspective-card') ) {
+            if ( e.target.classList.contains(elClass) ) {
                 self.perspective(e, e.target);
             }
         });
@@ -53,12 +100,13 @@ function perspectiveHover(el, intensity) {
 
     for (var i = 0; i < els.length; i++ ) {
 
-        els[i].addEventListener('mouseleave', function(e){
-            self.reset(this);
+        els[i].addEventListener('mouseleave', function(){
+            // the issue here is that we are presented with a string as opposed to values
+            self.getTransforms(this.style.transform);
         });
 
     }
     
 }
 
-perspectiveHover('.js-perspective-card', 7);
+perspectiveHover('.js-perspective-card', '.js-perspective', 15);
